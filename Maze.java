d3dc6db2965e9cccd1bfae0ff78d90e58ece49a9
rkhuda <rkhuda@stuy.edu>
2018-01-07 12:40:56 -0500

    made separate border class

--- Maze.java
+++ Maze.java
@@ -12,47 +12,21 @@ import javax.swing.*;
 //import javax.swing.JFrame;
 //import javax.swing.JPanel;
 
-public class Maze extends JPanel {
+public class Maze extends JFrame {
 
-    public void paint(Graphics g) {
+    private Walls border;
 
-	Graphics2D g2 = (Graphics2D) g;
-	g2.setStroke(new BasicStroke(10));
-	g2.setPaint(Color.blue);
-	g2.draw(new Rectangle2D.Float(0, 0, 500, 500));
+    public Maze(){
+	this.border = new Walls();
     }
 
     public static void main(String[] args){
-	JFrame frame = new JFrame();
+        Maze frame = new Maze();
+	frame.setTitle("Pacman");
 	frame.setBackground(Color.black);
-	frame.add(new Maze());
+	frame.getContentPane().add(frame.border);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setBounds(500, 200, 500, 520);
 	frame.setVisible(true);
     }
-    /*
-    public Maze() {
-
-	JFrame frame = new JFrame("MazeFoundation");
-	frame.setTitle("Pacman");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	Container pane = frame.getContentPane();
-
-	JPanel panel = new JPanel(new GridLayout(3, 0));
-	for (int i = 1; i <= 9; i++) {
-	    panel.add(new JButton("Button " + i));
-	}
-	panel.setBackground(Color.black);
-	pane.add(panel, BorderLayout.CENTER);
-
-	frame.pack();
-	frame.setVisible(true);
-    }
-
-    public static void main(String[] args){
-	Maze maze = new Maze();
-    }
-    */
-    
 }
