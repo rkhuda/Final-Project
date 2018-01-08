5744be5dbc4b1c382653530a43403ece597925b3
rkhuda <rkhuda@stuy.edu>
2018-01-08 18:36:14 -0500

    code is up to date with branch

--- Maze.java
+++ Maze.java
@@ -1,32 +1,73 @@
 import java.awt.*;
-import java.awt.geom.Line2D;
-import java.awt.geom.Rectangle2D;
-
-//import java.awt.Graphics;
-//import java.awt.BorderLayout;
-//import java.awt.Container;
-//import java.awt.GridLayout;
+import java.awt.event.*;
 
 import javax.swing.*;
-//import javax.swing.JButton;
-//import javax.swing.JFrame;
-//import javax.swing.JPanel;
+import javax.swing.border.*;
 
 public class Maze extends JFrame {
 
-    private Walls border;
+    private Container pane;
+    private JPanel[][] panels;
+
+    private ImageIcon Pacman;
+    private ImageIcon Blinky;
+    private ImageIcon Pinky;
+    private ImageIcon Inky;
+    private ImageIcon Clyde;
+    
 
     public Maze(){
-	border = new Walls();
-    }
+	
+	this.setTitle("Pacman");
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.setBounds(300, 200, 900, 900);
 
+	pane = getContentPane();
+
+	Pacman = new ImageIcon("Pacman.png");
+	Image image = Pacman.getImage(); // transform it
+	Image newimg = image.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
+	Pacman = new ImageIcon(newimg);  // transform it back
+
+	Blinky = new ImageIcon("Blinky.png");
+	Image image2 = Blinky.getImage();
+	Image newimage = image2.getScaledInstance(23, 23, java.awt.Image.SCALE_SMOOTH);
+	Blinky = new ImageIcon(newimage);
+
+	pane.setLayout(new GridLayout(40,40));
+	panels = new JPanel[40][40];
+	for (int x = 0; x < panels.length;x++) {
+            for (int y = 0; y < panels[x].length;y++) {
+
+		panels[x][y] = new JPanel(new GridLayout(1,1));
+		JPanel p = panels[x][y];
+		JLabel b = new JLabel();
+	       	Container c = getContentPane();
+		
+		if (x == 25 && y == 20) {
+                    b.setIcon(Pacman);
+                    p.setBackground(Color.BLACK);
+		}
+		else if (x == 0 && y == 0) {
+			b.setIcon(Blinky);
+			p.setBackground(Color.BLACK);
+		    }
+		else{
+		    p.setBackground(Color.BLACK);
+		}
+		
+		p.add(b);
+		pane.add(p);
+		b.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+	    }
+
+	}
+ }
+    
     public static void main(String[] args){
-        Maze maze = new Maze();
-	maze.setTitle("Pacman");
-	maze.setBackground(Color.black);
-	maze.getContentPane().add(maze.border);
-	maze.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	maze.setBounds(500, 200, 500, 520);
+	Maze maze = new Maze();
 	maze.setVisible(true);
     }
-}
+ }
+    
+
