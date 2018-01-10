1932e7eee64e6cd44e053519533198a4b625bca3
Michael Luo <mluo1@stuy.edu>
2018-01-09 23:02:07 -0500

    pacman class with get methods

---
+++ Pacman.java
@@ -0,0 +1,15 @@
+public class Pacman{
+    private int xcor;
+    private int ycor;
+    public Pacman(int xcor,int ycor){
+	this.xcor = xcor;
+	this.ycor = ycor;
+    }
+    public int getX(){
+	return xcor;
+    }
+    public int getY(){
+	return ycor;
+    }
+}
+ 
