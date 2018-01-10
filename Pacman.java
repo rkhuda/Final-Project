a4822607657ade6b6a53f8973c1e15aa579de7e8
Michael Luo <mluo1@stuy.edu>
2018-01-10 00:28:55 -0500

    added keyListener events--change with coordinates

--- Pacman.java
+++ Pacman.java
@@ -6,16 +6,18 @@ import javax.swing.*;
 public class Pacman{
     private int xcor;
     private int ycor;
-    private ImageIcon image;
+    private int xChange;
+    private int yChange;
+    private Image image;
     
 
     public Pacman(){
 	xcor = 20;
 	ycor = 20;
-	image = new ImageIcon("Pacman.png");
-	Image image2 = image.getImage();
-	Image newimg = image.getScaledInstance(23,23,java.awt.Image.SCALE_SMOOTH);
-    	image = new ImageIcon(newimg);
+	ImageIcon image2 = new ImageIcon("Pacman.png");
+	image = image2.getImage();
+	//	Image newimg = image3.getScaledInstance(23,23,java.awt.Image.SCALE_SMOOTH);
+	//	image = newimg.getImage();
     }
 
 
@@ -32,6 +34,52 @@ public class Pacman{
     public void setY(int ycor){
 	this.ycor = ycor;
     }
-    
-    
+    public Image getImage(){
+	return image;
+    }
+
+    public void move(){
+	xcor += xChange;
+	ycor += yChange;
+    }
+    public void keyPressed(KeyEvent e){
+	int c = e .getKeyCode();
+	if(c == KeyEvent.VK_UP){
+	    yChange = 1;
+	}
+	if(c == KeyEvent.VK_DOWN){
+	    yChange = -1;
+	}
+	if(c == KeyEvent.VK_RIGHT){
+	    xChange = 1;
+	}
+	if(c == KeyEvent.VK_LEFT){
+	    xChange = -1;
+	}
+    }
+
+    public void keyTyped(KeyEvent e){
+    }
+
+    public void keyReleased(KeyEvent e){
+	int c = e .getKeyCode();
+	if(c == KeyEvent.VK_UP){
+	    yChange = 0;
+	}
+	if(c == KeyEvent.VK_DOWN){
+	    yChange = 0;
+	}
+	if(c == KeyEvent.VK_RIGHT){
+	    xChange = 0;
+	}
+	if(c == KeyEvent.VK_LEFT){
+	    xChange = 0;
+	}
+    }
+
 }
+
+
+
+
+
