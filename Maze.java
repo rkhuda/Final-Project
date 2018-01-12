0b765185f720617c05a484b13be1be860abac67d
Michael Luo <mluo1@stuy.edu>
2018-01-11 20:07:14 -0500

    pacman can move up but multiple icons remain

--- Maze.java
+++ Maze.java
@@ -4,69 +4,90 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class Maze extends JFrame {
+public class Maze extends JFrame implements KeyListener {
 
     private Container pane;
     private JPanel[][] panels;
-
     private ImageIcon Pacman;
-    private ImageIcon Blinky;
-    private ImageIcon Pinky;
-    private ImageIcon Inky;
-    private ImageIcon Clyde;
-    
-
+    private Pacman pac;
     public Maze(){
 	
 	this.setTitle("Pacman");
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	this.setBounds(300, 200, 900, 900);
-
+	addKeyListener(this);
 	pane = getContentPane();
-
+	
 	Pacman = new ImageIcon("Pacman.png");
 	Image image = Pacman.getImage(); // transform it
 	Image newimg = image.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
 	Pacman = new ImageIcon(newimg);  // transform it back
-
-	Blinky = new ImageIcon("Blinky.png");
-	Image image2 = Blinky.getImage();
-	Image newimage = image2.getScaledInstance(23, 23, java.awt.Image.SCALE_SMOOTH);
-	Blinky = new ImageIcon(newimage);
-
+       	pac = new Pacman();
 	pane.setLayout(new GridLayout(40,40));
 	panels = new JPanel[40][40];
 	for (int x = 0; x < panels.length;x++) {
             for (int y = 0; y < panels[x].length;y++) {
-
-		panels[x][y] = new JPanel(new GridLayout(1,1));
+                panels[x][y] = new JPanel(new GridLayout(1,1));
 		JPanel p = panels[x][y];
 		JLabel b = new JLabel();
 	       	Container c = getContentPane();
 		
-		if (x == 25 && y == 20) {
-                    b.setIcon(Pacman);
-                    p.setBackground(Color.BLACK);
+		    
+		
+		if (x == 20 && y == 20) {
+		    //      pac.setX(20);
+		    //        pac.setY(20);
+		      b.setIcon(Pacman);
+		    p.setBackground(Color.WHITE);
+		   
+
 		}
-		else if (x == 0 && y == 0) {
-			b.setIcon(Blinky);
-			p.setBackground(Color.BLACK);
-		    }
 		else{
 		    p.setBackground(Color.BLACK);
 		}
-		
 		p.add(b);
 		pane.add(p);
-		b.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 	    }
 
 	}
- }
+    }
+    
+    public void keyPressed(KeyEvent e){
+	int c = e.getKeyCode();
+	JLabel b = new JLabel();
+	
+		if(c == KeyEvent.VK_UP){
+		    b.setIcon(null);
+		}
+		if(c == KeyEvent.VK_UP){
+		    // b.setIcon(Pacman);
+		      pac.setY(pac.getY() - 1);
+		      //  panels[pac.getY()][pac.getX()].add(b);
+ panels[pac.getY()][pac.getX()].add(b);
+	 b.setIcon(Pacman);
+		      
+		      	    panels[20][21].setBackground(Color.BLUE);
+		    //	    panels[20][22].setBackground(Color.WHITE);
+		         System.out.println(c);
+	    
+		}
+    
+	
+    }
+		
+
+    public void keyTyped(KeyEvent e){
+    }
+    public void keyReleased(KeyEvent e){
+	//	JLabel b = new JLabel();
+	//	(panels[pac.getX()][pac.getY()]).setIcon(Pacman);
+	//	b.setIcon(Pacman);
+    }
     
     public static void main(String[] args){
 	Maze maze = new Maze();
 	maze.setVisible(true);
+	//	maze.pack();
     }
  }
     
