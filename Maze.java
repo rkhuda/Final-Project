f37bc373d7aa0c8016cc4ffe91f88cb3951e1f88
Michael Luo <mluo1@stuy.edu>
2018-01-12 01:39:46 -0500

    fixed weird code that happened

--- Maze.java
+++ Maze.java
@@ -25,29 +25,14 @@ public class Maze extends JFrame {
 	pane = getContentPane();
 
 	Pacman = new ImageIcon("Pacman.png");
-	Image pac = Pacman.getImage(); // transform it
-	Image scaledpac = pac.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
-	Pacman = new ImageIcon(scaledpac);  // transform it back
+	Image image = Pacman.getImage(); // transform it
+	Image newimg = image.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
+	Pacman = new ImageIcon(newimg);  // transform it back
 
 	Blinky = new ImageIcon("Blinky.png");
-	Image blinky = Blinky.getImage();
-	Image scaledB = blinky.getScaledInstance(23, 23, java.awt.Image.SCALE_SMOOTH);
-	Blinky = new ImageIcon(scaledB);
-
-	Pinky = new ImageIcon("Pinky.png");
-	Image pinky = Pinky.getImage();
-	Image scaledP = pinky.getScaledInstance(23, 23, java.awt.Image.SCALE_SMOOTH);
-	Pinky = new ImageIcon(scaledP);
-
-	Inky = new ImageIcon("Inky.png");
-	Image inky = Inky.getImage();
-	Image scaledI = inky.getScaledInstance(23, 23, java.awt.Image.SCALE_SMOOTH);
-	Inky = new ImageIcon(scaledI);
-
-	Clyde = new ImageIcon("Clyde.png");
-	Image clyde = Clyde.getImage();
-	Image scaledC = clyde.getScaledInstance(23, 23, java.awt.Image.SCALE_SMOOTH);
-	Clyde = new ImageIcon(scaledC);
+	Image image2 = Blinky.getImage();
+	Image newimage = image2.getScaledInstance(23, 23, java.awt.Image.SCALE_SMOOTH);
+	Blinky = new ImageIcon(newimage);
 
 	pane.setLayout(new GridLayout(40,40));
 	panels = new JPanel[40][40];
@@ -65,18 +50,6 @@ public class Maze extends JFrame {
 		}
 		else if (x == 0 && y == 0) {
 			b.setIcon(Blinky);
-			p.setBackground(Color.BLACK);
-		    }
-		else if (x == 0 && y == 1) {
-			b.setIcon(Pinky);
-			p.setBackground(Color.BLACK);
-		    }
-		else if (x == 1 && y == 0) {
-			b.setIcon(Inky);
-			p.setBackground(Color.BLACK);
-		    }
-		else if (x == 1 && y == 1) {
-			b.setIcon(Clyde);
 			p.setBackground(Color.BLACK);
 		    }
 		else{
