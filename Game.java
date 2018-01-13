832003beb5519e81675e024001c8cefe9425e97e
rkhuda <rkhuda@stuy.edu>
2018-01-13 10:24:46 -0500

    tried to experiment w/ code

--- Game.java
+++ Game.java
@@ -2,13 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Game {
-
+    
     public static void main(String[] args) {
         JFrame frame = new JFrame("Pacman");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(300, 200, 900, 900);
-        frame.add(new Ghost());
-        frame.setVisible(true);
+
+	Ghost character = new Ghost();
+        frame.add(character);
+
+	frame.setVisible(true);
     }
 
 }
