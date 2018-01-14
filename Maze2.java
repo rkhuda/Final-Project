78c85425abb42cf0d981cf5fa87f2e7bfef45ff5
Michael Luo <mluo1@stuy.edu>
2018-01-14 16:39:37 -0500

    inky moves

--- Maze2.java
+++ Maze2.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
-
+import javax.swing.Timer;
 import javax.swing.*;
 import javax.swing.border.*;
 
@@ -8,182 +8,160 @@ public class Maze2 extends JFrame implements KeyListener {
 
     private Container pane;
     private JPanel[][] panels;
-    private JLabel con;
-    private JLabel b;
-
-    private ImageIcon R;
-    private ImageIcon L;
-    private ImageIcon U;
-    private ImageIcon D;
-
+    private ImageIcon Pacman;
+    private ImageIcon Inky;
     private Pacman pac;
+    private Enemy blue;
+    private JLabel con;
+    private Timer timerblue;
+    private JLabel pm;
+    private JLabel bg;
 
     public Maze2(){
 	
 	this.setTitle("Pacman");
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	this.setBounds(400, 100, 700, 700);
-	this.setResizable(false);
-	
+	this.setBounds(300, 200, 900, 900);
 	addKeyListener(this);
 	pane = getContentPane();
-
-	R = new ImageIcon("PacmanRight.png");
-	Image imageR = R.getImage(); // transform it
-	Image newimgR = imageR.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
-	R = new ImageIcon(newimgR);  // transform it back
-
-        L = new ImageIcon("PacmanLeft.png");
-	Image imageL = L.getImage(); // transform it
-	Image newimgL = imageL.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
-	L = new ImageIcon(newimgL);  // transform it back
-
-        D = new ImageIcon("PacmanDown.png");
-	Image imageD = D.getImage(); // transform it
-	Image newimgD = imageD.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
-	D = new ImageIcon(newimgD);  // transform it back
-
-        U = new ImageIcon("PacmanUp.png");
-	Image imageU = U.getImage(); // transform it
-	Image newimgU = imageU.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
-	U = new ImageIcon(newimgU);  // transform it back
-
-	b = new JLabel();
+       	
+	Pacman = new ImageIcon("Pacman.png");
+	Image image = Pacman.getImage(); // transform it
+	Image newimg = image.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
+	Pacman = new ImageIcon(newimg);  // transform it back
+	pm = new JLabel();
        	pac = new Pacman();
-	pane.setLayout(new GridLayout(25, 25));
 
-	panels = new JPanel[25][25];
+	Inky = new ImageIcon("Inky.png");
+	Image image2 = Inky.getImage(); // transform it
+	Image newimg2 = image2.getScaledInstance(23,23,  java.awt.Image.SCALE_SMOOTH);
+	Inky = new ImageIcon(newimg2);  // transform it back
+	bg = new JLabel();
+        blue = new Enemy();
+
+	
+	 
+	   
+	
+	pane.setLayout(new GridLayout(40 ,40 ));
+	panels = new JPanel[40 ][40];
 	for (int x = 0; x < panels.length;x++) {
             for (int y = 0; y < panels[x].length;y++) {
                 panels[x][y] = new JPanel(new GridLayout(1,1));
+		
 		JPanel p = panels[x][y];
-	       	Container c = getContentPane();
-		
-		if (x == pac.getY() && y == pac.getX()) {
-		    b.setIcon(R);
+	      	Container c = getContentPane();			    
+	 	if (x == pac.getY() && y == pac.getX()) {
+      		    pm.setIcon(Pacman);
 		    p.setBackground(Color.BLACK);
-		    p.add(b);
+		    p.add(pm);
 		}
-
-		p.setBackground(Color.BLACK);
-		pane.add(p);
-
-		if (x == 0) {
-		    p.setBorder(BorderFactory.createMatteBorder(27, 0, 0, 0, Color.BLUE));
+		if(x == blue.getY() && y == blue.getX()){
+		    bg.setIcon(Inky);
+		    p.add(bg);
 		}
-		if (x == 24) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 0, 27, 0, Color.BLUE));
-		}
-		if (y == 0 && (x != 0 && x != 24)) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if (y == 24 && (x != 0 && x != 24)) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 28, Color.BLUE));
-		}
-		if ((y == 2 || y == 22) && (x == 2 || x == 3 || x == 4 || x == 6 || x == 8 ||
-			       x == 9 || x == 10 || x == 12 || x == 13 || x == 14 || x == 16 ||
-			       x == 18 || x == 20 || x == 22)) {
-		     p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 3 || y == 21) && (x == 2 || x == 6 || x == 14 || x == 16 || x == 18 ||
-					    x == 20 || x == 22)){
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 4 || y == 20) && (x == 2 || x == 4 || x == 6 || x == 8 || x == 9 ||
-					    x == 10 || x == 11 || x == 12 || x == 14 ||
-					    x == 18 || x == 22)) {
-		     p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 5 || y == 19) && (x == 4 || x == 12 || x == 16 || x == 17 || x == 18 ||
-					    x == 20 || x == 21 || x == 22)){
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 6 || y == 18) && (x == 2 || x == 3|| x == 4 || x == 6 || x == 7||
-					    x == 9 || x == 10 || x == 11 || x == 12 || x == 13 ||
-					    x == 14)) {
-		     p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 7 || y == 17) && (x == 7 || x == 12 || x == 16 || x == 17 || x == 18 ||
-					    x == 20 || x == 21 || x == 22)) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 8 || y == 16) && (x == 2 || x == 3|| x == 4 || x == 5|| x == 7 ||
-					    x == 9 || x == 10 || x == 12 || x == 13 ||
-					    x == 22)) {
-		     p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 9 || y == 15) && (x == 2 || x == 7 || x == 16 || x == 17 || x == 18 ||
-					    x == 19 || x == 20 || x == 22)) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 10 || y == 14) && (x == 2 || x == 4 || x == 5 || x == 13  ||
-					     x == 22)) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if ((y == 11 || y == 13) && (x == 7 || x == 13 || x == 15 || x == 16 ||
-					     x == 18 || x == 20)) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if (y == 12 && (x == 1 || x == 2 || x == 4 || x == 5 || x == 6 || x == 7 ||
-				x == 13 || x == 14 || x == 15 || x == 16 || x == 18 || x == 20 ||
-				x == 22 || x == 23)) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
+	       
+	        p.setBackground(Color.BLACK);
+     		pane.add(p);
 		
-		//make center box
-		if ((x == 9 || x == 11) && (y == 10 || y == 11 ||  y == 13 ||
-					    y == 14)) {
-		    p.setBorder(BorderFactory.createMatteBorder(28, 0, 0, 0, Color.BLUE));
-		}
-		if (x == 10 && (y == 10 || y == 14)) {
-		    p.setBorder(BorderFactory.createMatteBorder(0, 28, 0, 0, Color.BLUE));
-		}
-		if (x == 11 && y == 12) {
-		     p.setBorder(BorderFactory.createMatteBorder(28, 0, 0, 0, Color.BLUE));
-		}
 	    }
 	}
+
+       	ActionListener BlueGhost = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+		//	 moveUpBlue();
+		//	moveDownBlue();
+		//	moveRightBlue();
+			moveLeftBlue();
+                System.out.println("Reading SMTP Info.");
+            }
+	    };
+        Timer timer = new Timer(100, BlueGhost);
+        timer.setRepeats(true);
+        timer.start();
+
+
+
+    
     }
 
+    public void moveUpBlue(){
+	if(blue.getY()- 1 >= 0){
+	    bg.setIcon(null);
+	    panels[blue.getY()][blue.getX()].repaint();
+	    blue.setY(blue.getY() - 1);
+	    panels[blue.getY()][blue.getX()].add(bg);
+	    bg.setIcon(Inky);
+	}
+    }
+    public void moveDownBlue(){
+	if(blue.getY() + 1 <= 39){
+	    bg.setIcon(null);
+	    panels[blue.getY()][blue.getX()].repaint();
+	    blue.setY(blue.getY() + 1);
+	    panels[blue.getY()][blue.getX()].add(bg);
+	    bg.setIcon(Inky);
+	}
+    }
+    public void moveRightBlue(){
+	if(blue.getX()+ 1 <= 39){
+	    bg.setIcon(null);
+	    panels[blue.getY()][blue.getX()].repaint();
+	    blue.setX(blue.getX() + 1);
+	    panels[blue.getY()][blue.getX()].add(bg);
+	    bg.setIcon(Inky);
+	}
+    }
+    public void moveLeftBlue(){
+	if(blue.getX()- 1 >= 0){
+	    bg.setIcon(null);
+	    panels[blue.getY()][blue.getX()].repaint();
+	    blue.setX(blue.getX() - 1);
+	    panels[blue.getY()][blue.getX()].add(bg);
+	    bg.setIcon(Inky);
+	}
+    }
+  
     
     public void keyPressed(KeyEvent e){
 	int c = e.getKeyCode();
 	//	 b = new JLabel();
 	
 	
-	if(c == KeyEvent.VK_UP && pac.getY() - 1 > 0){
-	    b.setIcon(null);
+	if(c == KeyEvent.VK_UP && pac.getY() - 1 >= 0){
+	    pm.setIcon(null);
 	    panels[pac.getY()][pac.getX()].repaint();
 	    pac.setY(pac.getY() - 1);
-	    panels[pac.getY()][pac.getX()].add(b);
-	    b.setIcon(U);
+	    panels[pac.getY()][pac.getX()].add(pm);
+	    pm.setIcon(Pacman);
 	    System.out.println(pac.getY());
 	    
 	    // b.setLocation(1,1);
  	}
     
-	if(c == KeyEvent.VK_DOWN && pac.getY() + 1 < 39){
-	    b.setIcon(null);
+	if(c == KeyEvent.VK_DOWN && pac.getY() + 1 <= 39){
+	    pm.setIcon(null);
 	    panels[pac.getY()][pac.getX()].repaint();
 	    pac.setY(pac.getY() + 1);
-	    panels[pac.getY()][pac.getX()].add(b);
-	    b.setIcon(D);
+	    panels[pac.getY()][pac.getX()].add(pm);
+	    // panels[pac.getY()][pac.getX()].setBackground(Color.WHITE);
+	    pm.setIcon(Pacman);
 	    System.out.println(pac.getY());
 	}
-	if(c == KeyEvent.VK_RIGHT && pac.getX() + 1 < 39){
-	    b.setIcon(null);
+	if(c == KeyEvent.VK_RIGHT && pac.getX() + 1 <= 39){
+	    pm.setIcon(null);
 	    panels[pac.getY()][pac.getX()].repaint();
 	    pac.setX(pac.getX() + 1);
-	    panels[pac.getY()][pac.getX()].add(b);
-	    b.setIcon(R);
+	    panels[pac.getY()][pac.getX()].add(pm);
+	    pm.setIcon(Pacman);
 	    System.out.println(pac.getX());
 	}
-	if(c == KeyEvent.VK_LEFT && pac.getX() - 1 > 0){
-       	    b.setIcon(null);
+	if(c == KeyEvent.VK_LEFT && pac.getX() - 1 >= 0){
+       	    pm.setIcon(null);
 	    panels[pac.getY()][pac.getX()].repaint();
 	    pac.setX(pac.getX() - 1);
-	    panels[pac.getY()][pac.getX()].add(b);
-	    b.setIcon(L);
+	    panels[pac.getY()][pac.getX()].add(pm);
+	    pm.setIcon(Pacman);
 	    System.out.println(pac.getX());
 	}
     }
@@ -201,3 +179,5 @@ public class Maze2 extends JFrame implements KeyListener {
 	//	maze.pack();
     }
 }
+    
+
